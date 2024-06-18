e787f162f53fa52ae900b2a57bbd5d79af592a30
OME <kolbybob111@gmail.com>
2024-06-18 17:57:18 +0700

    change

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import NavbarComponent from './components/NavbarComponent.vue';
+import Features from './components/Features.vue';
+import Footers from './components/Footers.vue'
+
+</script>
+
+<template>
+  <NavbarComponent/>
+  <Features/>
+  <Footers/>
+  
+  
+
+</template>
